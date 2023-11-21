51847bfbabf444d8a4525a33d2ad9198ec533bcd
Giovanni <gtpepi@proton.me>
2023-11-21 18:42:12 -0300

    Initial commit

---
+++ src/test/java/org/giovanni/MicroServicesMacroGains/MicroServicesMacroGainsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.giovanni.MicroServicesMacroGains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicesMacroGainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
