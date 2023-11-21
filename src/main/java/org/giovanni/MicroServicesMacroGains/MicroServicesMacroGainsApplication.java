42d6b84400232cf98752095138a6ab0578bcefd3
Giovanni <gtpepi@proton.me>
2023-11-21 18:42:12 -0300

    Initial commit

---
+++ src/main/java/org/giovanni/MicroServicesMacroGains/MicroServicesMacroGainsApplication.java
@@ -0,0 +1,13 @@
+package org.giovanni.MicroServicesMacroGains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesMacroGainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicesMacroGainsApplication.class, args);
+	}
+
+}
